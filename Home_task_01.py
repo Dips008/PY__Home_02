aa875a53504bbd287647cd9ffd9d0f39f73ac197
Igor_Demchenko <idemch777@gmail.com>
2022-12-24 10:38:25 +0300

    Home_task_01

--- Home_task_01.py
+++ Home_task_01.py
@@ -3,3 +3,17 @@
 #  Пример:
 #  - 6782 -> 23
 #  - 0,56 -> 11
+
+# Var_1
+# str_number = str(input('Enter a real number = '))
+# suma = 0
+# for i in str_number:
+#     if i.isdigit():
+#         suma += int(i)
+# print(' - ', str_number, ' -> ', suma)
+
+# Var_2
+str_number = str(input('Enter a real number = '))
+print(' - ', str_number, ' -> ', end='')
+str_number = str_number.replace('.', '')
+print(sum(map(int, list(str_number))))
